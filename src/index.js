16319990a7b774c02256214c4711fd2d408014b0
Sajid Ansari <sajidansari33272@gmail.com>
2023-08-05 18:03:54 +0545

    Ladder import

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import "./App.css";
 import App from "./App";
 import "react-tippy/dist/tippy.css";
-import "react-loading-skeleton/dist/skeleton.css";
 import "react-toastify/dist/ReactToastify.css";
+import "react-loading-skeleton/dist/skeleton.css";
 
 ReactDOM.render(
   <React.StrictMode>
